4cdcc2528cf24d567a39a052a41cac3045ea76d2
npasquie <nicolas.pasquier.pro@gmail.com>
2020-03-22 23:41:01 +0100

    wip

--- back.js
+++ back.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true},
+mongoose.connect('mongodb://mongo:27017/test', {useNewUrlParser: true, useUnifiedTopology: true},
     () => {console.log("connection established")})
     .catch(error => handleError(error));
 let db = mongoose.connection;
